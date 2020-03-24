b6152925dc2d1371fc554b48aff2ef880cbaf928
Jacob Colyvan <jacob_colyvan@keemail.me>
2020-03-24 19:09:09 +1100

    Testing for note prompts to audio functionality. NOTE: non-working code is commented out.
    "musicTesting.rb" is a file containing tests for using the Muse gem.

--- notePrompt.rb
+++ notePrompt.rb
@@ -1,35 +1,53 @@
 require 'tty-prompt'
 require 'terminal-table'
 require 'muse'
+include Muse
 
 
 
 notesHash = {
-    0=>"a", 1 => "A#", 2=> "B", 3 => "C", 4=>"C#", 5=>"D", 
-    6=>"D#", 7=>"E", 8=>"F", 9=>"F#", 10=>"G", 11=>"G#"
+    0=>"a", 1 => "a#", 2=> "b", 3 => "c", 4=>"c#", 5=>"d", 
+    6=>"d#", 7=>"e", 8=>"f", 9=>"f#", 10=>"g", 11=>"g#"
 }
 $prompt = TTY::Prompt.new
 
 
 
 def musicTable(notesHash)
-    
     howManyNotes = $prompt.ask("Pass me how many notes you want:  ").to_i
-    notes = []
+    $notes = []
 
 
     for i in 1..howManyNotes 
         seed = rand(12)
-        notes.push(notesHash[seed])
+        $notes.push(notesHash[seed])
     end
 
     rows = []
-    rows << notes
+    rows << $notes
     return Terminal::Table.new :rows => rows
 end
 
-
-
-
 puts musicTable(notesHash)
+# p $notes
+
+# songBar = ""
+
+# for i in 0..$notes.length-1
+#     if $notes[i].include?("#")
+#         $notes[i] = $notes[i][0]+ "is"
+#     end
+#     songBar << $notes[i]+"4; "
+# end
+# songBar << ""
+# puts songBar
+# # stack = proc { do_this }
+# $bar = Proc.new  { b4; a4; gis4; a4; } 
+
+# Song.record 'promptSong' do
+#     bar(1,b:0.25, bpm:50).notes (&$bar)
+# end
+# titleMusic = fork{ exec 'afplay', "./promptSong.wav" }
+
+
 
